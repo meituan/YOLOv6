806070358e9b5896250939b5ee1a931b8c9a0986
Chilicyy <lichuyi@meituan.com>
2022-06-23 11:52:22 +0800

    fix typo

--- configs/yolov6s_finetune.py
+++ configs/yolov6s_finetune.py
@@ -1,7 +1,7 @@
 # YOLOv6s model
 model = dict(
     type='YOLOv6s',
-    pretrained='weights/yolo6s.pt',
+    pretrained='./weights/yolov6s.pt',
     depth_multiple=0.33,
     width_multiple=0.50,
     backbone=dict(
