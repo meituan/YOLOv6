6642dee72027a9dd346637934d90d1311b9d9742
MTChengMeng <chengmeng05@meituan.com>
2022-06-29 20:11:48 +0800

    1. Modify cache logic: When dataset changed, cache file will be re-generated; 2. Modify the code logic of identifying whether coco dataset is used; 3. Changes of config file; 4. Some format changes.

--- yolov6/data/data_load.py
+++ yolov6/data/data_load.py
@@ -11,47 +11,83 @@ from yolov6.utils.events import LOGGER
 from yolov6.utils.torch_utils import torch_distributed_zero_first
 
 
-def create_dataloader(path, img_size, batch_size, stride, hyp=None, augment=False, check_images=False, check_labels=False, pad=0.0, rect=False, rank=-1, workers=8, shuffle=False,class_names=None, task='Train'):
-    '''Create general dataloader.
+def create_dataloader(
+    path,
+    img_size,
+    batch_size,
+    stride,
+    hyp=None,
+    augment=False,
+    check_images=False,
+    check_labels=False,
+    pad=0.0,
+    rect=False,
+    rank=-1,
+    workers=8,
+    shuffle=False,
+    data_dict=None,
+    task="Train",
+):
+    """Create general dataloader.
 
     Returns dataloader and dataset
-    '''
+    """
     if rect and shuffle:
-        LOGGER.warning('WARNING: --rect is incompatible with DataLoader shuffle, setting shuffle=False')
+        LOGGER.warning(
+            "WARNING: --rect is incompatible with DataLoader shuffle, setting shuffle=False"
+        )
         shuffle = False
     with torch_distributed_zero_first(rank):
-        dataset = TrainValDataset(path, img_size, batch_size,
-                                  augment=augment,
-                                  hyp=hyp,
-                                  rect=rect,
-                                  check_images=check_images,
-                                  stride=int(stride),
-                                  pad=pad,
-                                  rank=rank,
-                                  class_names=class_names,
-                                  task=task)
+        dataset = TrainValDataset(
+            path,
+            img_size,
+            batch_size,
+            augment=augment,
+            hyp=hyp,
+            rect=rect,
+            check_images=check_images,
+            check_labels=check_labels,
+            stride=int(stride),
+            pad=pad,
+            rank=rank,
+            data_dict=data_dict,
+            task=task,
+        )
 
     batch_size = min(batch_size, len(dataset))
-    workers = min([os.cpu_count() // int(os.getenv('WORLD_SIZE', 1)), batch_size if batch_size > 1 else 0, workers])  # number of workers
-    sampler = None if rank == -1 else distributed.DistributedSampler(dataset, shuffle=shuffle)
-    return TrainValDataLoader(dataset,
-                  batch_size=batch_size,
-                  shuffle=shuffle and sampler is None,
-                  num_workers=workers,
-                  sampler=sampler,
-                  pin_memory=True,
-                  collate_fn=TrainValDataset.collate_fn), dataset
+    workers = min(
+        [
+            os.cpu_count() // int(os.getenv("WORLD_SIZE", 1)),
+            batch_size if batch_size > 1 else 0,
+            workers,
+        ]
+    )  # number of workers
+    sampler = (
+        None if rank == -1 else distributed.DistributedSampler(dataset, shuffle=shuffle)
+    )
+    return (
+        TrainValDataLoader(
+            dataset,
+            batch_size=batch_size,
+            shuffle=shuffle and sampler is None,
+            num_workers=workers,
+            sampler=sampler,
+            pin_memory=True,
+            collate_fn=TrainValDataset.collate_fn,
+        ),
+        dataset,
+    )
 
 
 class TrainValDataLoader(dataloader.DataLoader):
-    """ Dataloader that reuses workers
+    """Dataloader that reuses workers
 
     Uses same syntax as vanilla DataLoader
     """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        object.__setattr__(self, 'batch_sampler', _RepeatSampler(self.batch_sampler))
+        object.__setattr__(self, "batch_sampler", _RepeatSampler(self.batch_sampler))
         self.iterator = super().__iter__()
 
     def __len__(self):
@@ -63,7 +99,7 @@ class TrainValDataLoader(dataloader.DataLoader):
 
 
 class _RepeatSampler:
-    """ Sampler that repeats forever
+    """Sampler that repeats forever
 
     Args:
         sampler (Sampler)
