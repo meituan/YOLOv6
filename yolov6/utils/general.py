9f90384954c534df30d06ada19af14a133d1ea15
hadoop-automl <hadoop-automl@set-zw-yarn-training21.mt>
2022-06-29 16:11:16 +0800

    fix_bug_exp_name

---
+++ yolov6/utils/general.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+
+def increment_name(path):
+    path = Path(path)
+    sep = ''
+    if path.exists():
+        path, suffix = (path.with_suffix(''), path.suffix) if path.is_file() else (path, '')
+        for n in range(1, 9999):
+            p = f'{path}{sep}{n}{suffix}'
+            if not os.path.exists(p):
+                break
+        path = Path(p)
+    return path
