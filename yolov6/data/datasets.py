f3ff04f4d5248729f91634b609e062de91093cf9
1chimaruGin <kyitharhein18@gmail.com>
2022-07-05 19:30:07 +0630

    Added video support, read and write

--- yolov6/data/datasets.py
+++ yolov6/data/datasets.py
@@ -2,13 +2,14 @@
 # -*- coding:utf-8 -*-
 
 import glob
+from io import UnsupportedOperation
 import os
 import os.path as osp
 import random
 import json
 import time
 import hashlib
-
+from pathlib import Path
 from multiprocessing.pool import Pool
 
 import cv2
@@ -29,6 +30,7 @@ from yolov6.utils.events import LOGGER
 
 # Parameters
 IMG_FORMATS = ["bmp", "jpg", "jpeg", "png", "tif", "tiff", "dng", "webp", "mpo"]
+VID_FORMATS = ["mp4", "mov", "avi", "mkv"]
 # Get orientation exif tag
 for k, v in ExifTags.TAGS.items():
     if v == "Orientation":
@@ -548,3 +550,63 @@ class TrainValDataset(Dataset):
         assert isinstance(paths, list), "Only support list currently."
         h = hashlib.md5("".join(paths).encode())
         return h.hexdigest()
+
+class LoadData:
+    def __init__(self, path):
+        p = str(Path(path).resolve())  # os-agnostic absolute path
+        if os.path.isdir(p):
+            files = sorted(glob.glob(os.path.join(p, '*.*')))  # dir
+        elif os.path.isfile(p):
+            files = [p]  # files
+        else:
+            raise FileNotFoundError(f'Invalid path {p}')
+        
+        imgp = [i for i in files if i.split('.')[-1] in IMG_FORMATS]
+        vidp = [v for v in files if v.split('.')[-1] in VID_FORMATS]
+        self.files = imgp + vidp
+        self.nf = len(self.files)
+        self.type = 'image'
+        if any(vidp):
+            self.add_video(vidp[0])  # new video
+        else:
+            self.cap = None
+
+    @staticmethod
+    def checkext(path):
+        file_type = 'image' if path.split('.')[-1].lower() in IMG_FORMATS else 'video'
+        return file_type
+
+    def __iter__(self):
+        self.count = 0
+        return self
+
+    def __next__(self):
+        if self.count == self.nf:
+            raise StopIteration
+        path = self.files[self.count]
+
+        if self.checkext(path) == 'video':
+            self.type = 'video'
+            ret_val, img = self.cap.read()
+            while not ret_val:
+                self.count += 1
+                self.cap.release()
+                if self.count == self.nf:  # last video
+                    raise StopIteration
+                path = self.files[self.count]
+                self.add_video(path)
+                ret_val, img = self.cap.read()
+        else:
+            # Read image
+            self.count += 1
+            img = cv2.imread(path)  # BGR
+
+        return img, path, self.cap
+    
+    def add_video(self, path):
+        self.frame = 0
+        self.cap = cv2.VideoCapture(path)
+        self.frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    def __len__(self):
+        return self.nf  # number of files
