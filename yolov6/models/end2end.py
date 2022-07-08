4a1594bad71d7174e5e0cd45580d47e06a7a4778
jianghongliang02 <jianghongliang02@meituan.com>
2022-07-08 16:03:10 +0800

    standardize the codes of end2end onnx export

--- yolov6/models/end2end.py
+++ yolov6/models/end2end.py
@@ -135,17 +135,17 @@ class ONNX_TRT(nn.Module):
 
 class End2End(nn.Module):
 
-    def __init__(self, model, max_obj=100, iou_thres=0.45, score_thres=0.25, max_wh=None, device=None, preprocess=False):
+    def __init__(self, model, max_obj=100, iou_thres=0.45, score_thres=0.25, max_wh=None, device=None, with_preprocess=False):
         super().__init__()
         device = device if device else torch.device('cpu')
-        self.preprocess = preprocess
+        self.with_preprocess = with_preprocess
         self.model = model.to(device)
         self.patch_model = ONNX_TRT if max_wh is None else ONNX_ORT
         self.end2end = self.patch_model(max_obj, iou_thres, score_thres, max_wh, device)
         self.end2end.eval()
 
     def forward(self, x):
-        if self.preprocess:
+        if self.with_preprocess:
             x = x[:,[2,1,0],...]
             x = x * (1/255)
         x = self.model(x)
