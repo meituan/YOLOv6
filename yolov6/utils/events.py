ecba1a7688c2d5a88d663541adaa0e0dd6600e3f
czla <zlchen@tongji.edu.cn>
2022-07-19 14:57:14 +0800

    Add train_batch/val_predictions visualization to tensorboard

--- yolov6/utils/events.py
+++ yolov6/utils/events.py
@@ -39,3 +39,14 @@ def write_tblog(tblogger, epoch, results, losses):
     tblogger.add_scalar("train/l1_loss", losses[1], epoch + 1)
     tblogger.add_scalar("train/obj_loss", losses[2], epoch + 1)
     tblogger.add_scalar("train/cls_loss", losses[3], epoch + 1)
+
+
+def write_tbimg(tblogger, imgs, step, type='train'):
+    """Display train_batch and validation predictions to tensorboard."""
+    if type == 'train':
+        tblogger.add_image(f'train_batch', imgs, step + 1, dataformats='HWC')
+    elif type == 'val':
+        for idx, img in enumerate(imgs):
+            tblogger.add_image(f'val_img_{idx + 1}', img, step + 1, dataformats='HWC')
+    else:
+        LOGGER.warning('WARNING: Unknown image type to visualize.\n')
