130c870b8906eeee4ed9150d064b5a1295cb7259
shensheng272 <108787191+shensheng272@users.noreply.github.com>
2022-09-02 17:43:46 +0800

    fix warning typo

--- yolov6/core/evaler.py
+++ yolov6/core/evaler.py
@@ -247,8 +247,8 @@ class Evaler:
         '''Check whether confidence and iou threshold are best for task val/speed'''
         if task != 'train':
             if task == 'val':
-                if conf_thres > 0.01:
-                    LOGGER.warning(f'The best conf_thresh when evaluate the model is less than 0.01, while you set it to: {conf_thres}')
+                if conf_thres > 0.03:
+                    LOGGER.warning(f'The best conf_thresh when evaluate the model is less than 0.03, while you set it to: {conf_thres}')
                 if iou_thres != 0.65:
                     LOGGER.warning(f'The best iou_thresh when evaluate the model is 0.65, while you set it to: {iou_thres}')
             if task == 'speed' and conf_thres < 0.25:
