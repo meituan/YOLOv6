9dee24b84d9c46a9ae3dd4666ec835723df7f5d8
梁雨菲 <liangyufei@a.sh.vip.dianping.com>
2022-09-13 21:00:27 +0800

    fix resume optimizer bug

--- yolov6/core/engine.py
+++ yolov6/core/engine.py
@@ -286,7 +286,6 @@ class Trainer:
         if self.main_process:
             LOGGER.info(f'\nTraining completed in {(time.time() - self.start_time) / 3600:.3f} hours.')
             save_ckpt_dir = osp.join(self.save_dir, 'weights')
-            strip_optimizer(save_ckpt_dir, self.epoch)  # strip optimizers for saved pt model
         if self.device != 'cpu':
             torch.cuda.empty_cache()
 
