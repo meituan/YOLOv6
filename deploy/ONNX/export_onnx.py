e07f6e718fc6e23c5c2550945e8b7729baeef209
asaf.b@lvision.io <asaf.b@lvision.io>
2022-11-09 10:01:16 +0200

    exporting to onnx

--- deploy/ONNX/export_onnx.py
+++ deploy/ONNX/export_onnx.py
@@ -22,8 +22,8 @@ from io import BytesIO
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default='./yolov6s.pt', help='weights path')
-    parser.add_argument('--img-size', nargs='+', type=int, default=[640, 640], help='image size')  # height, width
+    parser.add_argument('--weights', type=str, default='./runs/train/exp2 (with heavy augmentation)/weights/best_stop_aug_ckpt.pt', help='weights path')
+    parser.add_argument('--img-size', nargs='+', type=int, default=[416, 416], help='image size')  # height, width
     parser.add_argument('--batch-size', type=int, default=1, help='batch size')
     parser.add_argument('--half', action='store_true', help='FP16 half-precision export')
     parser.add_argument('--inplace', action='store_true', help='set Detect() inplace=True')
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     parser.add_argument('--ort', action='store_true', help='export onnx for onnxruntime')
     parser.add_argument('--with-preprocess', action='store_true', help='export bgr2rgb and normalize')
     parser.add_argument('--topk-all', type=int, default=100, help='topk objects for every images')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='iou threshold for NMS')
+    parser.add_argument('--iou-thres', type=float, default=0.75, help='iou threshold for NMS')
     parser.add_argument('--conf-thres', type=float, default=0.4, help='conf threshold for NMS')
     parser.add_argument('--device', default='0', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     args = parser.parse_args()
