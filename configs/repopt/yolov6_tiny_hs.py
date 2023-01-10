7fa9ba9fe14a2c471e7501ff345cdb0f0c3ae725
yili123123 <yiliyili123@outlook.com>
2023-01-10 11:50:33 +0800

    Fix bug of training V6-RepOpt: no attribute 'atss_warmup_epoch'

--- configs/repopt/yolov6_tiny_hs.py
+++ configs/repopt/yolov6_tiny_hs.py
@@ -22,6 +22,7 @@ model = dict(
         anchors=1,
         out_indices=[17, 20, 23],
         strides=[8, 16, 32],
+        atss_warmup_epoch=0,
         iou_type='siou',
         use_dfl=False,
         reg_max=0 #if use_dfl is False, please set reg_max to 0
