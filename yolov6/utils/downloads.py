2100b3d2eaa132f8f6e27a90cca0c34b2e9eed22
Kadir Nar <kadir.nar@hotmail.com>
2023-01-15 05:52:34 +0300

    Update downloads.py

--- yolov6/utils/downloads.py
+++ yolov6/utils/downloads.py
@@ -56,13 +56,5 @@ def attempt_download(file, repo='meituan/YOLOv6', release='0.3.0'):
                     tag = release
 
         file.parent.mkdir(parents=True, exist_ok=True)  # make parent dir (if required)
-        if name in assets:
-            url3 = 'https://drive.google.com/drive/folders/1EFQTEUeXWSFww0luse2jB9M1QNZQGwNl'  # backup gdrive mirror
-            safe_download(
-                file,
-                url=f'https://github.com/{repo}/releases/download/{tag}/{name}',
-                url2=f'https://storage.googleapis.com/{repo}/{tag}/{name}',  # backup url (optional)
-                min_bytes=1E5,
-                error_msg=f'{file} missing, try downloading from https://github.com/{repo}/releases/{tag} or {url3}')
     
     return str(file)
