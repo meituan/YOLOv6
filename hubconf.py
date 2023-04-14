b18c85ca97adbe51b17d82c72b5f8b116ef3844d
jianghongliang02 <jianghongliang02@meituan.com>
2023-04-14 17:53:34 +0800

    fix the error of rgb to bgr order convert.

--- hubconf.py
+++ hubconf.py
@@ -73,13 +73,14 @@ def process_image(path, img_size, stride):
     '''Preprocess image before inference.'''
     try:
             img_src = cv2.imread(path)
+            img_src = cv2.cvtColor(img_src, cv2.COLOR_RGB2BGR)
             assert img_src is not None, f"opencv cannot read image correctly or {path} not exists"
     except:
-            img_src = cv2.cvtColor(np.asarray(Image.open(path)), cv2.COLOR_RGB2BGR)
+            img_src = np.asarray(Image.open(path))
             assert img_src is not None, f"Image Not Found {path}, workdir: {os.getcwd()}"
 
     image = letterbox(img_src, img_size, stride=stride)[0]
-    image = image.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+    image = image.transpose((2, 0, 1)) # HWC to CHW
     image = torch.from_numpy(np.ascontiguousarray(image))
     image = image.float()
     image /= 255
