49ce3d64b6179aaa1393229ac83c51e4870e02f3
jianghongliang02 <jianghongliang02@meituan.com>
2023-05-15 15:16:17 +0800

    fix the bug of shape in random affine, add torch version check.

--- yolov6/data/data_augment.py
+++ yolov6/data/data_augment.py
@@ -78,7 +78,10 @@ def random_affine(img, labels=(), degrees=10, translate=.1, scale=.1, shear=10,
                   new_shape=(640, 640)):
     '''Applies Random affine transformation.'''
     n = len(labels)
-    height, width = new_shape
+    if isinstance(new_shape, int):
+        height = width = new_shape
+    else:
+        height, width = new_shape
 
     M, s = get_transform_matrix(img.shape[:2], (height, width), degrees, scale, shear, translate)
     if (M != np.eye(3)).any():  # image changed
