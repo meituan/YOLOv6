6f59716bd617f96b79accb4cfe7c694a2d791f27
Semih Demirel <85176438+semihhdemirel@users.noreply.github.com>
2023-07-08 18:06:43 +0300

    Update yolo_video.py

--- deploy/ONNX/OpenCV/yolo_video.py
+++ deploy/ONNX/OpenCV/yolo_video.py
@@ -53,26 +53,32 @@ def post_process(input_image, outputs):
         # Resizing factor.
         x_factor = image_width / INPUT_WIDTH
         y_factor =  image_height / INPUT_HEIGHT
-        # Iterate through detections.
-        for r in range(rows):
-            row = outputs[0][0][r]
-            confidence = row[4]
-            # Discard bad detections and continue.
-            if confidence >= CONFIDENCE_THRESHOLD:
-                    classes_scores = row[5:]
-                    # Get the index of max class score.
-                    class_id = np.argmax(classes_scores)
-                    #  Continue if the class score is above threshold.
-                    if (classes_scores[class_id] > SCORE_THRESHOLD):
-                        confidences.append(confidence)
-                        class_ids.append(class_id)
-                        cx, cy, w, h = row[0], row[1], row[2], row[3]
-                        left = int((cx - w/2) * x_factor)
-                        top = int((cy - h/2) * y_factor)
-                        width = int(w * x_factor)
-                        height = int(h * y_factor)
-                        box = np.array([left, top, width, height])
-                        boxes.append(box)
+    	# Iterate through 25200 detections.
+    	for r in range(rows):
+    		row = outputs[0][0][r]
+    		classes_scores = row[5:]
+    		max_score = np.amax(classes_scores)
+    
+    		# Discard bad detections and continue.
+    		if max_score >= CONFIDENCE_THRESHOLD:
+    			
+    			# Get the index of max class score.
+    			class_id = np.argmax(classes_scores)
+    
+    			#  Continue if the class score is above threshold.
+    			if (classes_scores[class_id] > SCORE_THRESHOLD):
+    				confidences.append(max_score)
+    				class_ids.append(class_id)
+    
+    				cx, cy, w, h = row[0], row[1], row[2], row[3]
+    
+    				left = int((cx - w/2) * x_factor)
+    				top = int((cy - h/2) * y_factor)
+    				width = int(w * x_factor)
+    				height = int(h * y_factor)
+    
+    				box = np.array([left, top, width, height])
+    				boxes.append(box)
 
         # Perform non maximum suppression to eliminate redundant, overlapping boxes with lower confidences.
         indices = cv2.dnn.NMSBoxes(boxes, confidences, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
